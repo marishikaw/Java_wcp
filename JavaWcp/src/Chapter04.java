faffb2bfbc78eefe9a1b0ac17aea604e0b90303d
marishikaw <90078966+marishikaw@users.noreply.github.com>
2021-12-29 13:30:46 +0900

    first commit

---
+++ JavaWcp/src/Chapter04.java
@@ -0,0 +1,11 @@
+
+public class Chapter04 {
+	public static void main(String[] args) {
+		final int LUCKEY_NUM = 1;
+		String str = "Marina Ishikawa";
+		System.out.println(str);
+		str = "sunny day";
+		System.out.println(str);
+		System.out.println(LUCKEY_NUM);
+	}
+}
---
+++ JavaWcp/src/Chapter05.java
@@ -0,0 +1,20 @@
+
+public class Chapter05 {
+	public static void main(String[] args) {
+		long long1 = 9223372036854775807L;
+		double double1 = 1234567.89;
+		boolean boolean1 = true;
+		Float float1 = 0.23f;
+		Integer int1 = 123;
+		Character car1 = 'A';
+		int[] array1 = new int[] { 10, 20, 30, 40, 50 };
+		
+		System.out.println(long1);
+		System.out.println(double1);
+		System.out.println(boolean1);
+		System.out.println(float1);
+		System.out.println(int1);
+		System.out.println(car1);
+		System.out.println(array1[2]);
+	}
+}
