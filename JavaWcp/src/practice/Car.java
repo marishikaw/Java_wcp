afd392c4e179b0b4e95e642d5c83f1a1ed1dda97
marishikaw <90078966+marishikaw@users.noreply.github.com>
2021-12-30 14:20:05 +0900

    [Add]Chapter10

--- JavaWcp/src/practice/Car.java
+++ JavaWcp/src/practice/Car.java
@@ -1,20 +1,22 @@
 package practice;
 
 public class Car {
-	private double fuelCost;
-	private double fuelAmount;
-	
-	public Car(double fuelCost, double fuelAmount) {
-		this.fuelCost = fuelCost;
-		this.fuelAmount = fuelAmount;
-	}
-	
-	public void move(int km) {
-		System.out.println(km + " km 走ります");
-		this.fuelAmount -= (km / fuelCost);
-	}
-	
-	public double getFuelAmount() {
-		return this.fuelAmount;
-	}
+    // 燃費（Km/L）
+    private double fuelCost;
+    // 残量（L）
+    private double fuelAmount;
+
+    public Car(double fuelCost, double fuelAmount) {
+        this.fuelCost = fuelCost;
+        this.fuelAmount = fuelAmount;
+    }
+
+    public void move(int km) {
+        System.out.println(km + " km 走ります");
+        this.fuelAmount -= (km / fuelCost);
+    }
+
+    public double getFuelAmount() {
+        return this.fuelAmount;
+    }
 }
--- JavaWcp/src/practice/Chapter09.java
+++ JavaWcp/src/practice/Chapter09.java
@@ -1,7 +1,7 @@
 package practice;
 
 public class Chapter09 {
-	public static void main(String[] args) {
+    public static void main(String[] args) {
         // 燃費（Km/L）
         double fuelCost = 12.5;
         // 残量（L）
